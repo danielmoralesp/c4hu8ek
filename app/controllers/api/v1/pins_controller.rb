05fac6a6bdf21f3c9084e0db50196b4464e33b6e
danielmoralesp <daniel_moralesp@hotmail.com>
2017-04-10 23:14:05 -0500

    solucion implementada

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::PinsController < ApplicationController
+
+  before_filter :restrict_access
+
   def index
     render json: Pin.all.order('created_at DESC')
   end
@@ -15,5 +18,10 @@ class Api::V1::PinsController < ApplicationController
   private
     def pin_params
       params.require(:pin).permit(:title, :image_url)
+    end
+
+    def restrict_access
+      user = User.find_by(email: params[:email])
+      head :unauthorized unless user && user.api_token == params[:api_token]
     end
 end
